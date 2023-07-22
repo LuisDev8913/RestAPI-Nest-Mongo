c29999a5672b9d345167f132653d247905666fea
Safronova Maryna <myjob.front.end@gmail.com>
2023-07-22 18:16:03 +0300

    init

---
+++ src/book/book.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schema/book.schema';
+
+@Controller('books')
+export class BookController {
+  constructor(private bookService: BookService) {}
+
+  @Get()
+  async getAllBooks(): Promise<Book[]> {
+    return await this.bookService.findAll();
+  }
+}
